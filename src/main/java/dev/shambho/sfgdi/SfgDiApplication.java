ec44c54418958509cd4e4d38abd8919d2271df98
Siva Krishna <shivataraya@gmail.com>
2020-07-10 19:18:40 -0500

    manual dependency injection

--- src/main/java/dev/shambho/sfgdi/SfgDiApplication.java
+++ src/main/java/dev/shambho/sfgdi/SfgDiApplication.java
@@ -1,6 +1,9 @@
 package dev.shambho.sfgdi;
 
+import dev.shambho.sfgdi.controllers.ConstructorInjectedController;
 import dev.shambho.sfgdi.controllers.MyController;
+import dev.shambho.sfgdi.controllers.PropertyInjectedController;
+import dev.shambho.sfgdi.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -16,6 +19,24 @@ public class SfgDiApplication {
         String mantra = myController.sayNamahSivaya();
 
         System.out.println(mantra);
+
+        System.out.println("---- Property");
+
+        PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+
+        System.out.println(propertyInjectedController.getGreeting());
+
+        System.out.println("------ Setter Based");
+
+        SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
+
+        System.out.println(setterInjectedController.getGreeting());
+
+        System.out.println("------- Constructor");
+
+        ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+
+        System.out.println(constructorInjectedController.getGreeting());
     }
 
 }
--- src/main/java/dev/shambho/sfgdi/controllers/PropertyInjectedController.java
+++ src/main/java/dev/shambho/sfgdi/controllers/PropertyInjectedController.java
@@ -1,9 +1,13 @@
 package dev.shambho.sfgdi.controllers;
 
 import dev.shambho.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class PropertyInjectedController {
 
+    @Autowired
     public GreetingService greetingService;
 
     public String getGreeting(){
